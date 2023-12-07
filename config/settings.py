7f446ff02b2cc660cd694a38c085c2fc1503714a
GeukYoung <rmrdud@gmail.com>
2023-12-07 10:38:44 +0900

    2-9

--- config/settings.py
+++ config/settings.py
@@ -111,6 +111,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+] # 스태틱 디렉터리 추가 및 폴더생성(필요)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
