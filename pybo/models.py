9fa6567445126dcfa5e5d3f8f8e32b21fd296a3c
GeukYoung <rmrdud@gmail.com>
2023-12-08 10:51:32 +0900

    3-07

--- pybo/models.py
+++ pybo/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Question(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     subject = models.CharField(max_length=200)
     content = models.TextField()
     create_date = models.DateTimeField()
@@ -10,6 +12,7 @@ class Question(models.Model):
         return self.subject
 
 class Answer(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     question = models.ForeignKey(Question, on_delete=models.CASCADE) # ForeignKey 속성: 다른모델과 연결, 'on_delete=models.CASCADE' 연결된 속성이 삭제되면 해당속성 함께 삭제.
     content = models.TextField()
     create_date = models.DateTimeField()
