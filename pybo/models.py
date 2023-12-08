d6a61058f2eac0b4dfaa3c04a3e5291339cdc42b
GeukYoung <rmrdud@gmail.com>
2023-12-08 11:11:28 +0900

    3-09

--- pybo/models.py
+++ pybo/models.py
@@ -7,7 +7,7 @@ class Question(models.Model):
     subject = models.CharField(max_length=200)
     content = models.TextField()
     create_date = models.DateTimeField()
-
+    modify_date = models.DateTimeField(null=True, blank=True)
     def __str__(self):
         return self.subject
 
@@ -16,3 +16,4 @@ class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE) # ForeignKey 속성: 다른모델과 연결, 'on_delete=models.CASCADE' 연결된 속성이 삭제되면 해당속성 함께 삭제.
     content = models.TextField()
     create_date = models.DateTimeField()
+    modify_date = models.DateTimeField(null=True, blank=True)
